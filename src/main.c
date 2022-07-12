ec7598df07bc5f90c851882b1d3a01e46ac83b01
o-dka <63544489+o-dka@users.noreply.github.com>
2022-07-12 20:42:08 +0300

    fixed some things

--- src/main.c
+++ src/main.c
@@ -8,19 +8,17 @@
 #include "types/types.h"
 #include "general_logic/general_logic.h"
 
-main() {
-  extern int deck_rand_count;
+int main() {
   int player_count = 2;	
   card_t cards[NUM];
   player_t players[player_count],*p_player = &players[0];
   fill_deck(cards);
   for (;;){
-    scanf("%d",player_count);
     for (int i = 0 ; i < player_count ;i++,p_player++){
-      shuffle_deck(cards,p_player,2); // randomise decks
+      shuffle_deck(cards,p_player,2); // randomise player sets
     }
     // implementation of rules
     print_cards(cards, 2); // cli graphix :3
   }
-  // return 0;
+  return 0;
 }
