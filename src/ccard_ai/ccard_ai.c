da3a0dec996041f5562d4510ff470ed286049596
o-dka <63544489+o-dka@users.noreply.github.com>
2022-07-14 13:57:47 +0300

    added a func, thats it :/

--- src/ccard_ai/ccard_ai.c
+++ src/ccard_ai/ccard_ai.c
@@ -2,6 +2,14 @@
 #include "ccard_ai.h"
 int set_sum (card_t *set){
   int sum = 0;
+  for (int i = 0; i < SET_MAX;i++){
+    if (set[i].val == -1){
+      break;
+    }
+    else{
+      sum+=set[i].val;
+    }
+  }
   return sum;
 }
 void give_up(void){}
