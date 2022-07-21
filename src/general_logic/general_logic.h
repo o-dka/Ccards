c698565de8144eaf16d85d572369f7fecb3bd350
odka <63544489+o-dka@users.noreply.github.com>
2022-07-22 02:47:44 +0300

    Update general_logic.h

--- src/general_logic/general_logic.h
+++ src/general_logic/general_logic.h
@@ -2,7 +2,7 @@
 #ifndef __GENERAL_LOGIC_H__
 #define __GENERAL_LOGIC_H__
 
-void print_cards(card_t *a, int it);
+void print_cards(card_t a, int it);
 void fill_deck(card_t *deck);
 int shuffle_deck(card_t *deck, player_t *p, int cards_to_add);
 int empt(card_t *p, int size);
